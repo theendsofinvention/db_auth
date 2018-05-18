cec115a25949a71d12f7474fb91f96fc59da47d6
132nd-etcher <132nd-etcher@daribouca.net>
2018-05-18 10:03:49 +0200

    here we go again

--- app.py
+++ app.py
@@ -2,36 +2,83 @@
 
 import os
 
+# from flask_api import FlaskAPI, request
+import jinja2
 from dropbox import DropboxOAuth2Flow
-from flask import Flask, redirect, request
+from flask import Flask, jsonify, make_response, redirect, request, session, url_for
 
-app = Flask(__name__)
+latex_jinja_env = jinja2.Environment(
+    block_start_string='\BLOCK{',
+    block_end_string='}',
+    variable_start_string='\VAR{',
+    variable_end_string='}',
+    comment_start_string='\#{',
+    comment_end_string='}',
+    line_statement_prefix='%%',
+    line_comment_prefix='%#',
+    trim_blocks=True,
+    autoescape=False,
+    loader=jinja2.FileSystemLoader(os.path.abspath('.'))
+)
+
+template = latex_jinja_env.get_template('index.html')
+
+from flask_bootstrap import Bootstrap
+
+app = Flask('db_auth')
+bootstrap = Bootstrap(app)
 app.secret_key = os.getenv('app_secret')
 
+TOKENS = {}
 
-def get_dropbox_auth_flow(web_app_session):
-    redirect_uri = 'https://esme-auth.herokuapp.com/dropbox/authorized'
+
+def get_dropbox_auth_flow():
+    redirect_uri = os.getenv('redirect_uri', None)
+    db_key = os.getenv('db_key', None)
+    db_secret = os.getenv('db_secret', None)
+    if not all([redirect_uri, db_key, db_secret]):
+        return make_response('', 404)
     return DropboxOAuth2Flow(
-        os.getenv('db_key'),
-        os.getenv('db_secret'),
+        db_key,
+        db_secret,
         redirect_uri,
-        web_app_session,
+        session,
         'dropbox-auth-csrf-token'
     )
 
 
 @app.route('/dropbox/login')
 def dropbox_auth_start():
-    authorize_url = get_dropbox_auth_flow({}).start()
-    return redirect(authorize_url)
+    authorize_url = get_dropbox_auth_flow().start()
+    user_id = session['dropbox-auth-csrf-token']
+    return jsonify({'user_id': user_id, 'authorize_url': authorize_url})
+
+
+@app.route('/tokens/')
+@app.route('/tokens/<uuid>')
+def tokens(uuid=None):
+    if uuid is None:
+        return make_response('', 404)
+    if uuid not in TOKENS:
+        return make_response('', 404)
+    token = TOKENS[uuid]
+    del TOKENS[uuid]
+    return jsonify({'token': token})
 
 
 @app.route('/dropbox/authorized')
 def dropbox_authorized():
+    global TOKENS
     code = request.args.get('code')
-    msg = f'Your token for using ESME with your Dropbox account is:\n\n{code}\n\nKeep it secret!'
-    return msg
+    uuid = request.args.get('state')
+    TOKENS[uuid] = code
+    return template.render(title='Authentication successful', code=code, app='ESME', provider='Dropbox')
+
+
+@app.route('/')
+def default_route():
+    return make_response('', 404)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
